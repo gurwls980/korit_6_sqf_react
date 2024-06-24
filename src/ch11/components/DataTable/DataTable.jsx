e94cd2b4554385eef2fdf4504acf0f2a3d169af2
gurwls980 <dingdong0907@naver.com>
2024-06-24 17:49:10 +0900

    0624 수업

--- src/ch11/components/DataTable/DataTable.jsx
+++ src/ch11/components/DataTable/DataTable.jsx
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataTableBody from "../DataTableBody/DataTableBody";
 import DataTableHeader from "../DataTableHeader/DataTableHeader";
 import "./style.css"
 import { SAMPLE_PRODUCTS } from "../../constants/sampleProducts";
 
 function DataTable() {
+    const [ isLoad, setLoad ] = useState(false);
     const [ mode, setMode ] = useState(0);  // 0 = 조회, 1 = 추가, 2 = 수정, 3 = 삭제
     const [ products, setProducts ] = useState([ ...SAMPLE_PRODUCTS ]);
+    const [ isDeleting, setDeleting ] = useState(false);
+
+    useEffect(() => {
+        const lsProducts = localStorage.getItem("products");
+        setProducts(!lsProducts ? [] : JSON.parse(lsProducts));
+        setLoad(true);
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("products", JSON.stringify(products));
+    }, [products])
 
     return (
         <div className="table-main-container">
-            <DataTableHeader mode={mode} setMode={setMode} />
-            <DataTableBody mode={mode} products={products} />
+            <DataTableHeader mode={mode} setMode={setMode} setProducts={setProducts} setDeleting ={setDeleting} />
+            <DataTableBody mode={mode} setMode={setMode} products={products} setProducts={setProducts} isDeleting={isDeleting} setDeleting={setDeleting} />
         </div>
     );
 }
